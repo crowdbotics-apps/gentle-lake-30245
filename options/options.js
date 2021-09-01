090e92823043aeacca3bf864ede8eab76732eba6
Crowdbotics <team@crowdbotics.com>
2021-09-01 12:29:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "gentle_lake_30245Identifier",
+  url: "https://gentle_lake_30245Identifier.botics.co",
+  api: "https://gentle_lake_30245Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
